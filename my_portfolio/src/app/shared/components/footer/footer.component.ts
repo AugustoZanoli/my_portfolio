ffd60ae528b03a4a3aeb2b15ce26ea7c34e62267
Zanoli <d2023001020@unifei.edu.br>
2026-03-30 09:55:37 -0300

    [FEAT] - Criação do footer

--- my_portfolio/src/app/shared/components/footer/footer.component.ts
+++ my_portfolio/src/app/shared/components/footer/footer.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  year = new Date().getFullYear();
 
+  links = [
+    { label: 'GitHub',   url: 'https://github.com/AugustoZanoli' },
+    { label: 'LinkedIn', url: 'https://www.linkedin.com/in/augusto-de-camargos-zanoli-9728b12a6/' },
+  ];
 }
